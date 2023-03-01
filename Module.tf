d70926e7991048f0e97528852d347de8148b2232
atiemwenjoseph <101192905+atiemwenjoseph@users.noreply.github.com>
2023-03-01 09:24:37 +0000

    Update Module.tf

--- Module.tf
+++ Module.tf
@@ -1,9 +1,10 @@
 module "sandbox" {
-    source = "./vpc-subbet-ec2"
+    source = "./Resources"
+    environment = "dev"
     block = "10.0.0.0/16"
     sub_block = "10.0.1.0/24"
     aws_regions = "eu-west-2"
     aws_regionss = "eu-west-2a"
-    ami = "ami-09ee0944866c73f62"
+    ami = "ami-0aaa5410833273cfe"
     instance = "t3.micro"
 }
